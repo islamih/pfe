d3c421a7efd5f62c694c6f3f4148f9cde415c36a
islamih <islamsmi.ih@gmail.com>
2020-03-02 16:51:45 +0100

    nv projet

---
+++ src/main/java/com/store/controller/HomeController.java
@@ -0,0 +1,20 @@
+package com.store.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+@RequestMapping("/")
+public String index() {
+	return "index";
+}
+
+@RequestMapping("/Myaccount")
+public String Myaccount() {
+	return "Myaccount";
+}
+	
+	
+
+}
---
+++ src/test/java/com/store/Store1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Store1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
